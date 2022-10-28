41627bb8447454772fc5d05e6f52e0ca2b17351a
Andrey-Matos <66771031+Andrey-Matos@users.noreply.github.com>
2022-10-28 13:50:42 -0300

    remove unused files and libs

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:ghibli_repository/provider/movie_provider.dart';
+//import 'package:ghibli_repository/provider/movie_provider.dart';
+import 'package:ghibli_repository/screens/home_screen.dart';
+//import 'package:ghibli_repository/screens/test.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp(ChangeNotifierProvider(
-    create: (context) => MovieProvider(),
-    child: homeScreen(),
-  ));
+  runApp(
+    MaterialApp(home: HomeScreen()),
+  );
 }
